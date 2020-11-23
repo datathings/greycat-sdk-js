246fc561b340e456611486672c2bfd230154c470
Maxime Tricoire <max.tricoire@gmail.com>
2020-11-23 15:05:22 +0100

    remove print-lib

--- packages/napi/src/print-lib.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import { Graph } from '.';
-
-const graph = new Graph();
-// tslint:disable-next-line:no-console
-console.log(JSON.stringify(graph.exportTypes()));
-graph.destroy();

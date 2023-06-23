ec7c6526d5cd5e6b3b545999cc3be01724b45675
Maxime Tricoire <maxime.tricoire@datathings.com>
2023-06-23 12:14:56 +0200

    add a test file for quick iteration

---
+++ test-project.js
@@ -0,0 +1,54 @@
+import { GreyCat } from './dist/esm/index.js';
+
+console.time('abi');
+const greycat = await GreyCat.init({
+  url: new URL('http://localhost:8080'),
+});
+console.timeEnd('abi');
+
+const fqns = [
+  'project::foo',
+  'project::int',
+  'project::float',
+  'project::bool',
+  'project::string',
+  'project::empty_array',
+  'project::int_array',
+  'project::float_array',
+  'project::map',
+  'project::throw',
+  'project::inline_object',
+  'project::person',
+  'project::enum_value',
+  'project::time',
+  'project::duration',
+  'project::geo',
+];
+
+for (const fqn of fqns) {
+  await call(fqn);
+}
+
+async function call(method) {
+  console.time('value');
+  try {
+    const value = await greycat.call(method);
+    if (Array.isArray(value)) {
+      console.log({ array: value });
+    } else if (value instanceof Map) {
+      const o = {};
+      value.forEach((value, key) => {
+        o[key] = value;
+      });
+      console.log({ map: o });
+    } else if (typeof value === 'object') {
+      console.log(JSON.parse(JSON.stringify(value)));
+    } else {
+      console.log({ [typeof value]: value });
+    }
+  } catch (err) {
+    console.log({ error: err });
+  } finally {
+    console.timeEnd('value');
+  }
+}

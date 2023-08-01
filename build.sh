03b770ae0cc94ef10b7deef4dccf2593ae842d13
Maxime Tricoire <maxime.tricoire@datathings.com>
2023-08-01 12:26:45 +0200

    ci stages

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -ex
 
+rm -rf dist
+
 pnpm install
 pnpm build
 
-mkdir -p dist2/sdk/js
+mkdir -p dist/sdk/js
 
 # sdk/js/greycat-sdk.tgz
 pnpm pack
-mv greycat-sdk*.tgz dist2/sdk/js/
+mv greycat-sdk*.tgz dist/sdk/js/package.tgz
 
 # sdk/js/greycat.sdk.min.js
-mv dist/bundle/greycat.sdk.min.js dist2/sdk/js/
+mv dist/bundle/greycat.sdk.min.js dist/sdk/js/
 
-rm -rf dist
-mv dist2 dist
+rm -rf dist/esm dist/bundle
---
+++ publish.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ex
+
+VERSION_MAJOR_MINOR=`cat VERSION`
+VERSION_SIMPLE=`git2version -s`
+
+echo "${VERSION_MAJOR_MINOR} / ${VERSION_SIMPLE}"
+
+sha256_hash=$(echo -n "$GET_GC_CI_PASS" | openssl dgst -sha256 | cut -d ' ' -f2)
+base64url_token=$(echo -n "ci:$sha256_hash" | base64 -w 0 )
+token=$(curl -s -d "[\"${base64url_token}\", false]" -X POST https://get.greycat.io/runtime::User::login | tr -d '"')
+
+cd dist
+
+file="dist.zip"
+zip -r $file sdk
+
+curl -s -X PUT -H "Authorization: $token" -T $file "https://get.greycat.io/files/sdk-js/${CI_COMMIT_REF_NAME}/${VERSION_MAJOR_MINOR}/${VERSION_SIMPLE}.zip"
+curl -s -X PUT -H "Authorization: $token" -T $file "https://get.greycat.io/files/sdk-js/${CI_COMMIT_REF_NAME}/latest.zip"

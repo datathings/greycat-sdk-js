ba7a761da21418162b861c359c66713c596f7bbd
Maxime Tricoire <maxime.tricoire@datathings.com>
2023-08-02 14:13:06 +0200

    trying package pub

--- publish.sh
+++ publish.sh
@@ -17,3 +17,4 @@ zip -r $file sdk
 
 curl -s -X PUT -H "Authorization: $token" -T $file "https://get.greycat.io/files/sdk/js/${CI_COMMIT_REF_NAME}/${VERSION_MAJOR_MINOR}/${VERSION_SIMPLE}.zip"
 curl -s -X PUT -H "Authorization: $token" -T $file "https://get.greycat.io/files/sdk/js/${CI_COMMIT_REF_NAME}/latest.zip"
+curl -s -X PUT -H "Authorization: $token" -T sdk/js/package.tgz "https://get.greycat.io/files/sdk/js/${CI_COMMIT_REF_NAME}/${VERSION_MAJOR_MINOR}/${VERSION_SIMPLE}-package.tgz"

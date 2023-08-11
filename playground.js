f517a3af45c52cae6e5a67e5742b05b8bb0ab5b4
François Fouquet <francois.fouquet@datathings.com>
2023-08-11 21:07:06 +0200

    test playground

--- playground.js
+++ playground.js
@@ -1,9 +1,38 @@
 
 import * as sdk from '@greycat/sdk';
-var g = await sdk.GreyCat.init({url:new URL("http://localhost:8080")});
-const newRole = sdk.std.runtime.UserRole.create(
-    g,
-    "role_1",
-    ["p1", "p2"],
-  );
-console.log(newRole.permissions);
+
+// var g = await sdk.GreyCat.init({url:new URL("http://localhost:8080")});
+// // const newRole = sdk.std.runtime.UserRole.create(
+// //     g,
+// //     "role_1",
+// //     ["p1", "p2"],
+// //   );
+// // console.log(newRole.permissions);
+
+// var p = await sdk.std.runtime.SecurityPolicy.permissions(g);
+// console.log(p);
+
+
+const greycat = await sdk.GreyCat.init({ url: new URL('http://localhost:8080') });
+
+const t = await greycat.call('project::whatever');
+
+//const t = await fetch('http://localhost:8080/project::whatever', { method: 'POST' });
+
+// const run = await greycat.call('runtime::Task::running');
+// console.log(run);
+
+//const run = await fetch('http://localhost:8080/runtime::Task::running', { method: 'POST' });
+
+const history = await greycat.call('runtime::Task::history', [0,100]);
+console.log(history);
+const history2 = await sdk.std.runtime.Task.history(greycat,0,100);
+console.log(history2);
+
+
+//console.log(t);
+
+//console.log((await t.json()) as sdk.std.runtime.Task);
+
+
+//console.log(await run.json());
